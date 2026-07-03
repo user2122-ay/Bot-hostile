01f014f5400f08f07258f10d2b4f8a28192f07e7
user2122-ay <anthonybatista2010@gmail.com>
2026-07-03 06:34:27 -0500

    Create ticketsConfig.js

---
+++ utils/ticketsConfig.js
@@ -0,0 +1,31 @@
+const DEPARTAMENTOS = {
+  fundacion: {
+    label: 'Fundación',
+    roleId: '1396174893892243660',
+    emoji: '<:owner:PON_AQUI_EL_ID>',
+    color: 0xf1c40f,
+  },
+  asuntos_internos: {
+    label: 'Asuntos Internos',
+    roleId: '1396174902465396827',
+    emoji: '<:AI2:PON_AQUI_EL_ID>',
+    color: 0xc0392b,
+  },
+  administracion: {
+    label: 'Administración',
+    roleId: '1396174907234193408',
+    emoji: '<:ADMIN2:PON_AQUI_EL_ID>',
+    color: 0xe67e22,
+  },
+  moderacion: {
+    label: 'Moderación',
+    roleId: '1396174915098513499',
+    emoji: '<:MOD:PON_AQUI_EL_ID>',
+    color: 0x3498db,
+  },
+};
+
+const CANAL_PANEL_ID = '1396617856275320863';
+const CANAL_REGISTROS_ID = '1522563718389436516';
+
+module.exports = { DEPARTAMENTOS, CANAL_PANEL_ID, CANAL_REGISTROS_ID };
