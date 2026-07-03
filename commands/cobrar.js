ce8303f78fadb1dfb01f1d9580217a9d222aad9d
user2122-ay <anthonybatista2010@gmail.com>
2026-07-02 21:16:22 -0500

    Update interaction reply flags in cobrar.js
    Removed ephemeral flag from interaction reply.

--- commands/cobrar.js
+++ commands/cobrar.js
@@ -54,7 +54,7 @@ module.exports = {
 
     await interaction.reply({
       components: [container],
-      flags: MessageFlags.IsComponentsV2 | MessageFlags.Ephemeral,
+      flags: MessageFlags.IsComponentsV2,
     });
   },
 };
