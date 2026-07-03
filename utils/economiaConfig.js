1691d157833c59d97823813908da52ff0c860acd
user2122-ay <anthonybatista2010@gmail.com>
2026-07-03 11:41:02 -0500

    Create economiaConfig.js

---
+++ utils/economiaConfig.js
@@ -0,0 +1,8 @@
+const IMPUESTO_TRANSFERENCIA_PORCENTAJE = 5; // % que se cobra al usar /pagar
+
+const CASINO = {
+  apuestaMinima: 10,
+  apuestaMaxima: 100000,
+};
+
+module.exports = { IMPUESTO_TRANSFERENCIA_PORCENTAJE, CASINO };
