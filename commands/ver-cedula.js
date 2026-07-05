5413984af3c2648ec9dadfdd237d7e93e4e6f04a
user2122-ay <anthonybatista2010@gmail.com>
2026-07-05 12:21:34 -0500

    Refactor ver-cedula command to use text response
    Removed image generation and attachment handling for cedula.

--- commands/ver-cedula.js
+++ commands/ver-cedula.js
@@ -1,12 +1,12 @@
-const { SlashCommandBuilder, ContainerBuilder, MessageFlags, AttachmentBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const Cedula = require('../models/Cedula');
-const { generarFrente, generarAtras } = require('../utils/cedulaImagen');
+const { construirMensajeFrente } = require('../utils/cedulaFlow');
 
 module.exports = {
   data: new SlashCommandBuilder().setName('ver-cedula').setDescription('Ver tu cédula de ciudadanía roleplay'),
 
   async execute(interaction) {
-    await interaction.deferReply({ ephemeral: true });
+    await interaction.deferReply(); // pública
 
     const cedula = await Cedula.findOne({ userId: interaction.user.id });
     if (!cedula) {
@@ -14,26 +14,7 @@ module.exports = {
       return;
     }
 
-    const bufferFrente = await generarFrente(cedula);
-    const bufferAtras = await generarAtras(cedula);
-
-    const archivoFrente = new AttachmentBuilder(bufferFrente, { name: 'cedula-frente.png' });
-    const archivoAtras = new AttachmentBuilder(bufferAtras, { name: 'cedula-atras.png' });
-
-    const container = new ContainerBuilder()
-      .setAccentColor(0x1f3a5f)
-      .addTextDisplayComponents((td) => td.setContent(`## 🪪 Cédula de ${cedula.nombres} ${cedula.apellidos}\nNIRP: ${cedula.nirp}`))
-      .addMediaGalleryComponents((gallery) =>
-        gallery.addItems(
-          (item) => item.setURL('attachment://cedula-frente.png'),
-          (item) => item.setURL('attachment://cedula-atras.png'),
-        ),
-      );
-
-    await interaction.editReply({
-      components: [container],
-      flags: MessageFlags.IsComponentsV2,
-      files: [archivoFrente, archivoAtras],
-    });
+    const mensaje = await construirMensajeFrente(cedula);
+    await interaction.editReply(mensaje);
   },
 };
