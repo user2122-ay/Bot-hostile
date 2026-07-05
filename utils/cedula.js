915c64178af23f532a8d48dffd29fb091eee5de8
user2122-ay <anthonybatista2010@gmail.com>
2026-07-05 12:20:31 -0500

    Refactor siguienteNirp to generate unique nirp

--- utils/cedula.js
+++ utils/cedula.js
@@ -1,12 +1,20 @@
-const CedulaContador = require('../models/CedulaContador');
+const Cedula = require('../models/Cedula');
+
+function generarNirpAleatorio() {
+  const numero = Math.floor(Math.random() * 1_000_000_000).toString().padStart(9, '0');
+  return `${numero.slice(0, 3)}.${numero.slice(3, 6)}.${numero.slice(6, 9)}`;
+}
 
 async function siguienteNirp() {
-  const doc = await CedulaContador.findOneAndUpdate(
-    { clave: 'nirp' },
-    { $inc: { contador: 1 } },
-    { upsert: true, new: true },
-  );
-  return `MR-${String(doc.contador).padStart(6, '0')}`;
+  let nirp;
+  let existe = true;
+  let intentos = 0;
+  while (existe && intentos < 15) {
+    nirp = generarNirpAleatorio();
+    existe = await Cedula.exists({ nirp });
+    intentos++;
+  }
+  return nirp;
 }
 
 module.exports = { siguienteNirp };
