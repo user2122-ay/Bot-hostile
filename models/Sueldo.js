3b65ba6b5bae8f1e337967158097906bce3ce234
user2122-ay <anthonybatista2010@gmail.com>
2026-07-06 08:14:19 -0500

    Add intervaloMinutos field to sueldoSchema

--- models/Sueldo.js
+++ models/Sueldo.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const sueldoSchema = new mongoose.Schema({
   roleId: { type: String, required: true, unique: true },
   monto: { type: Number, required: true },
+  intervaloMinutos: { type: Number, required: true, default: 60 },
 });
 
 module.exports = mongoose.model('Sueldo', sueldoSchema);
