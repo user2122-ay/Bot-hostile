d339b4dcd70bdd359387d68890de1b6614b2a912
user2122-ay <anthonybatista2010@gmail.com>
2026-07-06 08:27:32 -0500

    Update ROLE_ADMIN_ECONOMIA value

--- utils/permisos.js
+++ utils/permisos.js
@@ -1,4 +1,4 @@
-const ROLE_ADMIN_ECONOMIA = '1522423168323813376';
+const ROLE_ADMIN_ECONOMIA = '1459290551517184186';
 
 function esAdminEconomia(interaction) {
   return interaction.member.roles.cache.has(ROLE_ADMIN_ECONOMIA);
