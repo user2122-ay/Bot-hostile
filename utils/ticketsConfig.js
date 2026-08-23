9a9dc6bc893f33146ee4feb963d6cbdf1f392a2a
user2122-ay <anthonybatista2010@gmail.com>
2026-08-22 19:22:15 -0500

    Update ticketsConfig.js

--- utils/ticketsConfig.js
+++ utils/ticketsConfig.js
@@ -2,29 +2,29 @@ const DEPARTAMENTOS = {
   fundacion: {
     label: 'Fundación',
     roleId: '1396174899306954903',
-    emojiId: '1449546324906082314',
-    emojiName: 'owner',
+    emojiId: '1540790447738462288',
+    emojiName: 'asociacion',
     color: 0xf1c40f,
   },
   asuntos_internos: {
     label: 'Asuntos Internos',
     roleId: '1528573535440601178',
-    emojiId: '1458867618382352395',
-    emojiName: 'AI2',
+    emojiId: '1530964526219133018',
+    emojiName: 'herramientas',
     color: 0xc0392b,
   },
   administracion: {
     label: 'Administración',
     roleId: '1528573203537068072',
-    emojiId: '1458868143102497024',
-    emojiName: 'ADMIN2',
+    emojiId: '1530967352680910938',
+    emojiName: 'martillo',
     color: 0xe67e22,
   },
   moderacion: {
     label: 'Moderación',
     roleId: '1528573112055234640',
-    emojiId: '1458868676873687135',
-    emojiName: 'MOD',
+    emojiId: '1540834802448736256',
+    emojiName: 'miembro',
     color: 0x3498db,
   },
 };
@@ -41,7 +41,7 @@ function emojiMencion(dep) {
 function emojiSelect(dep) {
   return { id: dep.emojiId, name: dep.emojiName };
 }
-
+1
 // ¿Este member tiene el rol del departamento de ESE ticket? (para Reclamar)
 function esStaffDelDepartamento(member, dep) {
   return member.roles.cache.has(dep.roleId);
