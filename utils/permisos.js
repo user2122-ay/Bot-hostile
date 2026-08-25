1975be9a6f231ec1a7f9945f7eec2ad8b5375acf
user2122-ay <anthonybatista2010@gmail.com>
2026-08-24 21:28:06 -0500

    Refactor admin role handling in permisos.js

--- utils/permisos.js
+++ utils/permisos.js
@@ -1,7 +1,10 @@
-const ROLE_ADMIN_ECONOMIA = '1459290551517184186';
+// IDs de roles que pueden usar los comandos de staff de economía
+// (añadir/quitar dinero, sueldos, cambiar el emoji de la moneda, etc).
+// Para sumar otro rol con acceso, agregá su ID a este array.
+const ROLES_ADMIN_ECONOMIA = ['1459290551517184186'];
 
 function esAdminEconomia(interaction) {
-  return interaction.member.roles.cache.has(ROLE_ADMIN_ECONOMIA);
+  return interaction.member.roles.cache.some((rol) => ROLES_ADMIN_ECONOMIA.includes(rol.id));
 }
 
-module.exports = { ROLE_ADMIN_ECONOMIA, esAdminEconomia };
+module.exports = { ROLES_ADMIN_ECONOMIA, esAdminEconomia };
