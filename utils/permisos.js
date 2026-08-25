4ea6683095a006ede384f507c82e32c020e5644c
user2122-ay <anthonybatista2010@gmail.com>
2026-08-24 21:38:27 -0500

    Update permisos.js

--- utils/permisos.js
+++ utils/permisos.js
@@ -1,7 +1,7 @@
 // IDs de roles que pueden usar los comandos de staff de economía
 // (añadir/quitar dinero, sueldos, cambiar el emoji de la moneda, etc).
 // Para sumar otro rol con acceso, agregá su ID a este array.
-const ROLES_ADMIN_ECONOMIA = ['1459290551517184186'];
+const ROLES_ADMIN_ECONOMIA = ['1541637315640827974'];
 
 function esAdminEconomia(interaction) {
   return interaction.member.roles.cache.some((rol) => ROLES_ADMIN_ECONOMIA.includes(rol.id));
